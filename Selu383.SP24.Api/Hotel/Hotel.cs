62f2e80307b0c799e0930904d6a130e0092ed9a1
Cchris7 <85213942+Cchris7@users.noreply.github.com>
2024-01-31 17:49:55 -0600

    Declare as nullable

--- Selu383.SP24.Api/Hotel/Hotel.cs
+++ Selu383.SP24.Api/Hotel/Hotel.cs
@@ -3,14 +3,14 @@
     public class Hotel
     {
         public int Id { get; set; }
-        public required string Name { get; set; }
-        public required string Address { get; set; }
+        public required string? Name { get; set; }
+        public required string? Address { get; set; }
     }
 
     public class HotelDto
     {
         public int Id { get; set; }
-        public required string Name { get; set; }
-        public required string Address { get; set; }
+        public required string? Name { get; set; }
+        public required string? Address { get; set; }
     }
 }
