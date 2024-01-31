a889ccbebfde254a73bcf708f163a5f587baf3df
Cchris7 <85213942+Cchris7@users.noreply.github.com>
2024-01-31 17:41:59 -0600

    Can't contain null value

--- Selu383.SP24.Api/Hotel/Hotels.cs
+++ Selu383.SP24.Api/Hotel/Hotels.cs
@@ -3,14 +3,14 @@
     public class Hotels
     {
         public int Id { get; set; }
-        public string? Name { get; set; }
-        public string? Address { get; set; }
+        public string Name { get; set; }
+        public string Address { get; set; }
     }
 
     public class HotelsDto
     {
         public int Id { get; set; }
-        public string? Name { get; set; }
-        public string? Address { get; set; }
+        public string Name { get; set; }
+        public string Address { get; set; }
     }
 }
